de42e189b881253f75a3d33b6aaf60a23615f8ba
ouokki <ouokki.7zgui@gmail.com>
2022-11-15 20:07:33 +0100

    add feign : communication with microservice-produits from clientui

--- clientui/src/main/java/com/clientui/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/clientui/controller/ClientController.java
@@ -1,8 +1,12 @@
 package com.clientui.clientui.controller;
 
+import com.clientui.clientui.beans.ProductBean;
+import com.clientui.clientui.proxy.MicroserviceProduitsProxy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * @author Mohamed ouokki on 11/14/22
@@ -11,9 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ClientController {
 
+    private final MicroserviceProduitsProxy produitsProxy;
+
+    public ClientController(MicroserviceProduitsProxy produitsProxy){
+        this.produitsProxy = produitsProxy;
+    }
+
     @RequestMapping("/")
-    public String accueil(Model model)
-    {
+    public String accueil(Model model){
+        List<ProductBean> produits =  produitsProxy.listeDesProduits();
+        model.addAttribute("produits", produits);
         return "Accueil";
     }
 }
---
+++ clientui/src/main/java/com/clientui/clientui/proxy/MicroserviceProduitsProxy.java
@@ -0,0 +1,22 @@
+package com.clientui.clientui.proxy;
+
+import com.clientui.clientui.beans.ProductBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+/**
+ * @author Mohamed ouokki on 11/15/22
+ * @project microservice-commandes
+ */
+@FeignClient(name = "microservice-produits", url = "localhost:9001")
+public interface MicroserviceProduitsProxy {
+
+    @GetMapping(value = "/Produits")
+    List<ProductBean> listeDesProduits();
+
+    @GetMapping( value = "/Produits/{id}")
+    ProductBean recupererUnProduit(@PathVariable("id") int id);
+}
